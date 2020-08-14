5a5c5a75e6ef581dd06a78d4d1c6c1e61a1c4203
Natalia Denisenko <natdeni@google.com>
2020-08-14 10:22:24 +0000

    Add datastore for markers

--- src/main/java/com/google/sps/servlets/MapMarker.java
+++ src/main/java/com/google/sps/servlets/MapMarker.java
@@ -26,6 +26,9 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -38,6 +41,8 @@ import java.util.LinkedList;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.Iterator;
+import java.util.stream.Collectors;
+import org.json.JSONObject;
 
 import com.google.gson.Gson; 
 import com.google.gson.GsonBuilder;  
@@ -45,52 +50,25 @@ import com.google.gson.GsonBuilder;
 /** Servlet that process information about user's markers. */
 @WebServlet("/markers")
 public class MapMarker extends HttpServlet {
-  /** Processes GET request by returning list of marker coords in JSON format. */
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {// Create new Query for Commen objects.
-    Query query = new Query("Marker");
-
-    // Get access to dataStore.
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
-
-    Gson gson = new Gson();
-
-    // Get list of markers.
-    List<Marker> markersList = new ArrayList<>();
-    for (Entity entity : results.asIterable()) {
-      double lat = 123.0, lng = 123.0;
-      lat = (Double) entity.getProperty("lat");
-      lng = (Double) entity.getProperty("lng");
-
-      Marker marker = new Marker(lat, lng);
-      
-      markersList.add(marker);
-    }
-
-    // Convert list of comments to JSON format.
-    String json = gson.toJson(markersList);
-
-    // Return response to the request.
-    response.setContentType("application/json;");
-    response.getWriter().println(json);
-  }
 
   /** Processes POST request by storing received markers. */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Create DataStore entity.
-    // Entity markerEntity = new Entity("Marker");
+    // Read request body.
+    String requestBody = 
+      request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+    
+    // Convert it to JSON.
+    JSONObject jsonBody = new JSONObject(requestBody);
 
-    // markerEntity.setProperty("lat", Double.valueOf(request.getParameter("lat")));
-    // markerEntity.setProperty("lng", Double.valueOf(request.getParameter("lng")));
+    // Create new entity.
+    Entity markerEntity = new Entity("Marker");
+
+    markerEntity.setProperty("lat", jsonBody.getDouble("lat"));
+    markerEntity.setProperty("lng", jsonBody.getDouble("lng"));
 
     // Store new marker.
-    // DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    // datastore.put(markerEntity);
-
-    // Redirect back to the HTML page.
-    // response.sendRedirect("/index.html");
-    System.out.println("---------------Here-----------------");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(markerEntity);
   }
 }
