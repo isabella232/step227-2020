639d396943231d75ff6d8bb25ec8698d7d96d67d
Ioana Nedelcu <ioannanedelcu@google.com>
2020-09-23 21:04:07 +0000

    Fix formatting

--- src/main/java/com/google/sps/servlets/RouteImage.java
+++ src/main/java/com/google/sps/servlets/RouteImage.java
@@ -20,7 +20,6 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.sps.data.Images;
